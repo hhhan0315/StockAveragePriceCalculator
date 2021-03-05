803d1e603cb26c145a10d76e411013ace2e84889
hhhan0315 <hhan0315@naver.com>
2021-03-05 21:36:15 +0900

    키보드 상태에서 화면 아무곳이나 클릭 시 키보드 내리기 textfield delegate 설정

--- StockAveragePriceCalculator/ViewController.swift
+++ StockAveragePriceCalculator/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currentPriceField: UITextField!
+    @IBOutlet weak var currentAmountField: UITextField!
+    @IBOutlet weak var addPriceField: UITextField!
+    @IBOutlet weak var addAmountField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setDelegate()
+    }
+    
+    func setDelegate() {
+        currentPriceField.delegate = self
+        currentAmountField.delegate = self
+        addPriceField.delegate = self
+        addAmountField.delegate = self
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("hi")
+    }
 }
 
